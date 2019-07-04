1c928b9c2c29f6c6f160ced6f823f2055fec2889
Gabriel Monteiro Fernandes <mfgbriel92@gmail.com>
2019-07-03 22:33:14 -0300

    Módulo 06 - Estilizando formulário

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
-import { Container } from './styles'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import { Container, Form, Input, Button } from './styles'
 
 const Main = () => {
-  return <Container />
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Add user"
+        />
+        <Button>
+          <Icon name="add" size={18} color="#FFF" />
+        </Button>
+      </Form>
+    </Container>
+  )
 }
 
 export default Main
